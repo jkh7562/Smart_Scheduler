404fe924b70d6132b23c2afbc05cd9a3baa5f50e
JeeeeeongWoo <re7t7000@naver.com>
2024-03-14 20:39:54 +0900

    세팅

---
+++ src/main/java/com/example/smart_scheduler/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.smart_scheduler;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
