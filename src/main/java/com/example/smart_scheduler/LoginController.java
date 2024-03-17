58bd8a2c317c6081d7ab5df4296988bc25065e13
jkh7562 <86720157+jkh7562@users.noreply.github.com>
2024-03-17 09:38:47 +0900

    ë¡메인화면 병합전

--- src/main/java/com/example/smart_scheduler/LoginController.java
+++ src/main/java/com/example/smart_scheduler/LoginController.java
@@ -3,7 +3,7 @@ package com.example.smart_scheduler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class LoginController {
     @FXML
     private Label welcomeText;
 
