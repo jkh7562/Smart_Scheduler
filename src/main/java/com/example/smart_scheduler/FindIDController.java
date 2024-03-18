7303f2de85c8c9e6eb7c0fdd49c0ac628f74873b
jkh7562 <86720157+jkh7562@users.noreply.github.com>
2024-03-18 20:04:06 +0900

    뒤로가기구성

--- src/main/java/com/example/smart_scheduler/FindIDController.java
+++ src/main/java/com/example/smart_scheduler/FindIDController.java
@@ -1,4 +1,39 @@
 package com.example.smart_scheduler;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class FindIDController {
+    @FXML
+    private ImageView back_image;
+
+    @FXML
+    private void backimageClicked(MouseEvent event){
+        if(event.getSource() instanceof ImageView){
+            ImageView clickedImageView = (ImageView) event.getSource();
+            if (clickedImageView.getId().equals("back_image")){
+                try {
+                    // FXML 파일을 로드합니다.
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("Login_screen.fxml"));
+                    Parent root = loader.load();
+
+                    // 현재 Stage를 가져옵니다.
+                    Stage currentStage = (Stage) clickedImageView.getScene().getWindow();
+
+                    // 현재 Stage의 Scene을 새로운 Scene으로 교체합니다.
+                    currentStage.setScene(new Scene(root));
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
