89a4da417c6751e3fca9c49f44b16ca0f5d6fcce
jkh7562 <86720157+jkh7562@users.noreply.github.com>
2024-03-19 15:53:02 +0900

    ë로그인

--- src/main/java/com/example/smart_scheduler/LoginController.java
+++ src/main/java/com/example/smart_scheduler/LoginController.java
@@ -11,7 +11,13 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONObject;
 
 public class LoginController {
     @FXML
@@ -24,6 +30,8 @@ public class LoginController {
     private TextField id_textfield;
     @FXML
     private TextField pass_textfield;
+    @FXML
+    private Button login_button;
 
     @FXML
     private void signupLabelClicked(MouseEvent event){
@@ -94,6 +102,107 @@ public class LoginController {
         }
     }
 
+    @FXML
+    private void handleLoginButtonAction(ActionEvent event) {
+        String userID = id_textfield.getText();
+        String userPass = pass_textfield.getText();
+        if (event.getSource() instanceof Button) {
+            Button clickedButton = (Button) event.getSource();
+            if (clickedButton.getId().equals("login_button")) {
+                try {
+                    String serverURL = "http://hbr2024.dothome.co.kr/Login.php";
+                    URL url = new URL(serverURL);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("POST");
+                    connection.setDoOutput(true);
 
+                    String postData = "userID=" + userID + "&userPass=" + userPass;
+                    OutputStream os = connection.getOutputStream();
+                    os.write(postData.getBytes("UTF-8"));
+                    os.close();
+
+                    int responseCode = connection.getResponseCode();
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+                        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                        StringBuilder response = new StringBuilder();
+                        String inputLine;
+
+                        while ((inputLine = in.readLine()) != null) {
+                            response.append(inputLine);
+                        }
+
+                        System.out.println("Server Response: " + response.toString());
+
+                        JSONObject jsonResponse = new JSONObject(response.toString());
+                        boolean loginSuccess = jsonResponse.getBoolean("success");
+
+                        if (loginSuccess) {
+                            System.out.println("로그인 성공");
+
+                            FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
+                            Parent root = loader.load();
+
+                            // 현재 창을 닫음
+                            Stage stage = (Stage) clickedButton.getScene().getWindow();
+                            stage.setScene(new Scene(root));
+                            stage.show();
+
+                            connection.disconnect();
+
+                            try{
+
+                                String serverURL1 = "http://jkh75622.dothome.co.kr/primary.php"; // 서버 URL
+
+                                URL url1 = new URL(serverURL1);
+                                HttpURLConnection connection1 = (HttpURLConnection) url1.openConnection();
+                                connection1.setRequestMethod("POST");
+                                connection1.setDoOutput(true);
+
+                                String postData1 = "priid=" + userID + "&tableName = pri";
+                                OutputStream os1 = connection1.getOutputStream();
+                                os1.write(postData1.getBytes("UTF-8"));
+                                os1.close();
+
+                                int responseCode1 = connection1.getResponseCode();
+                                if (responseCode1 == HttpURLConnection.HTTP_OK) {
+                                    // 서버 응답 처리
+                                    BufferedReader in1 = new BufferedReader(new InputStreamReader(connection1.getInputStream()));
+                                    String response1;
+                                    while ((response1 = in1.readLine()) != null) {
+                                        System.out.println(response1);
+                                    }
+                                    in1.close();
+
+                                }else {
+                                    System.out.println("HTTP Error Code: " + responseCode1);
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+
+                        } else {
+                            System.out.println("로그인 실패");
+
+                            FXMLLoader loader = new FXMLLoader(getClass().getResource("loginfail.fxml"));
+                            Parent root = loader.load();
+                            Scene scene = new Scene(root);
+
+                            // 새로운 Stage를 생성하여 로그인 실패 창을 표시
+                            Stage failStage = new Stage();
+                            failStage.setScene(scene);
+                            failStage.show();
+                        }
+
+                    } else {
+                        System.out.println("HTTP Error Code: " + responseCode);
+                    }
+
+                    connection.disconnect();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
 }
