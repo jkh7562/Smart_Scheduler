ab7593dd139a27e4cbcd44886b90fb83ed2d9769
최종수 <cjskkkk@naver.com>
2024-03-19 23:04:04 +0900

    단기예보 api 받아오기

--- src/main/java/com/example/smart_scheduler/MainController.java
+++ src/main/java/com/example/smart_scheduler/MainController.java
@@ -10,20 +10,15 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
+
+
 public class MainController {
     public static void main(String[] args) throws IOException {
         // 오늘 날짜 가져오기
         LocalDate today = LocalDate.now();
 
         // 현재 시간 가져오기
-        LocalTime now = LocalTime.now();
-
-        // 현재 시간을 30분 간격으로 조정
-        if (now.getMinute() >= 30) {
-            now = now.plusHours(1).withMinute(0);
-        } else {
-            now = now.withMinute(30);
-        }
+        LocalTime now = LocalTime.of(20, 59); // 오늘 8시 59분
 
         // 날짜를 YYYYMMDD 형식으로 변환
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
@@ -42,12 +37,12 @@ public class MainController {
 
     // getApiResponse 함수는 주어진 baseDate와 baseTime에 해당하는 데이터를 가져옵니다.
     private static String getApiResponse(String baseDate, String baseTime) throws IOException {
-        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst"); /*URL*/
+        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"); /*URL*/
         urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=d7Mf01XLXPGGPlYQ8Itqst7R%2FFwiXNOaxoRkW39DZwzgXLW7SnyZ85l73m%2BOcmsY%2FXYWYQYjMQnbYJmNLHtD%2Fg%3D%3D"); /*Service Key*/
         urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("1000", "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("12", "UTF-8")); /*한 페이지 결과 수*/
         urlBuilder.append("&" + URLEncoder.encode("dataType", "UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*요청자료형식(XML/JSON) Default: XML*/
-        urlBuilder.append("&" + URLEncoder.encode("base_date", "UTF-8") + "=" + URLEncoder.encode(baseDate, "UTF-8")); /*‘21년 6월 28일 발표*/
+        urlBuilder.append("&" + URLEncoder.encode("base_date", "UTF-8") + "=" + URLEncoder.encode("20240319", "UTF-8")); /*‘21년 6월 28일 발표*/
         urlBuilder.append("&" + URLEncoder.encode("base_time", "UTF-8") + "=" + URLEncoder.encode(baseTime, "UTF-8")); /*06시 발표(정시단위) */
         urlBuilder.append("&" + URLEncoder.encode("nx", "UTF-8") + "=" + URLEncoder.encode("61", "UTF-8")); /*예보지점의 X 좌표값*/
         urlBuilder.append("&" + URLEncoder.encode("ny", "UTF-8") + "=" + URLEncoder.encode("110", "UTF-8")); /*예보지점의 Y 좌표값*/
@@ -73,3 +68,4 @@ public class MainController {
         return sb.toString(); // 반환 값을 추가합니다.
     }
 }
+
