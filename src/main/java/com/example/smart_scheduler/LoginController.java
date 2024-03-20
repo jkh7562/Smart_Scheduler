06e532b89cac7e5fb67f41d7fd06be532f85d883
jkh7562 <86720157+jkh7562@users.noreply.github.com>
2024-03-21 01:02:51 +0900

    자체로그인완성

--- src/main/java/com/example/smart_scheduler/LoginController.java
+++ src/main/java/com/example/smart_scheduler/LoginController.java
@@ -47,6 +47,12 @@ public class LoginController {
     }
 
     @FXML
+    private void initialize() {
+        // login_button에 대한 엔터 키 핸들러 등록
+        login_button.setDefaultButton(true);
+    }
+
+    @FXML
     private void signupLabelClicked(MouseEvent event){
         if(event.getSource() instanceof Label){
             Label clickedLabel = (Label) event.getSource();
