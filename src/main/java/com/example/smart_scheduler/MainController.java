1d3ff8e1c9eb2b4bdbeaed9203ab91dbe6341372
jkh7562 <86720157+jkh7562@users.noreply.github.com>
2024-03-21 01:02:51 +0900

    준호꺼합침

--- src/main/java/com/example/smart_scheduler/MainController.java
+++ src/main/java/com/example/smart_scheduler/MainController.java
@@ -1,35 +1,5 @@
 package com.example.smart_scheduler;
 
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
 public class MainController {
-    @FXML
-    Button work_button;
 
-    @FXML
-    private void workButtonAction(ActionEvent event) {
-        if (event.getSource() instanceof Button) {
-            Button clickedButton = (Button) event.getSource();
-            if (clickedButton.getId().equals("work_button")) {
-                try {
-                    FXMLLoader loader = new FXMLLoader(getClass().getResource("Main_Work.fxml"));
-                    Parent root = loader.load();
-                    Stage currentStage = (Stage) work_button.getScene().getWindow();
-                    currentStage.setScene(new Scene(root));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    // 사용자에게 오류 메시지 표시
-                }
-            }
-        }
-    }
 }
