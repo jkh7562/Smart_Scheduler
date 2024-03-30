847470b7e482ee89f2a4fc26e153187cb4010399
jkh7562 <86720157+jkh7562@users.noreply.github.com>
2024-03-31 04:51:23 +0900

    수정

--- src/main/java/com/example/smart_scheduler/WorkController.java
+++ src/main/java/com/example/smart_scheduler/WorkController.java
@@ -1,9 +1,23 @@
 package com.example.smart_scheduler;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
 
 public class WorkController {
 
     @FXML
-    private
+    private Pane Mon08,Mon09,Mon10,Mon11,Mon12,Mon13,Mon14,Mon15,Mon16,Mon17,Mon18,Mon19,Mon20,Mon21,Mon22;
+    @FXML
+    private Pane Tue08,Tue09,Tue10,Tue11,Tue12,Tue13,Tue14,Tue15,Tue16,Tue17,Tue18,Tue19,Tue20,Tue21,Tue22;
+    @FXML
+    private Pane Wen08,Wen09,Wen10,Wen11,Wen12,Wen13,Wen14,Wen15,Wen16,Wen17,Wen18,Wen19,Wen20,Wen21,Wen22;
+    @FXML
+    private Pane Thu08,Thu09,Thu10,Thu11,Thu12,Thu13,Thu14,Thu15,Thu16,Thu17,Thu18,Thu19,Thu20,Thu21,Thu22;
+    @FXML
+    private Pane Fri08,Fri09,Fri10,Fri11,Fri12,Fri13,Fri14,Fri15,Fri16,Fri17,Fri18,Fri19,Fri20,Fri21,Fri22;
+    @FXML
+    private Pane Sat08,Sat09,Sat10,Sat11,Sat12,Sat13,Sat14,Sat15,Sat16,Sat17,Sat18,Sat19,Sat20,Sat21,Sat22;
+    @FXML
+    private Pane Sun08,Sun09,Sun10,Sun11,Sun12,Sun13,Sun14,Sun15,Sun16,Sun17,Sun18,Sun19,Sun20,Sun21,Sun22;
+
 }
