50e6be73da49bf097338d184200253edae5f4f7f
jkh7562 <86720157+jkh7562@users.noreply.github.com>
2024-03-31 03:48:02 +0900

    규혁 병합

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.smart_scheduler {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.json;
+    requires javafx.web;
 
 
     opens com.example.smart_scheduler to javafx.fxml;
