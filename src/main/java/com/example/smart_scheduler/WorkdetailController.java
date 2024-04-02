be2d131847f83f53285f68379189b2fbe5e6921e
jkh7562 <86720157+jkh7562@users.noreply.github.com>
2024-04-02 16:51:18 +0900

    수정

--- src/main/java/com/example/smart_scheduler/WorkdetailController.java
+++ src/main/java/com/example/smart_scheduler/WorkdetailController.java
@@ -5,13 +5,21 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class WorkdetailController {
     @FXML
@@ -22,6 +30,10 @@ public class WorkdetailController {
     private MenuButton etime_button;
     @FXML
     private ImageView back_image;
+    @FXML
+    private Button add_button;
+    @FXML
+    private TextField content_textfield;
 
     @FXML
     private void weekSelection(ActionEvent event) {
@@ -51,6 +63,71 @@ public class WorkdetailController {
         // 현재 창을 닫습니다.
         currentStage.close();
     }
+    @FXML
+    private void addButtonAction(ActionEvent event) {
+        if (event.getSource() instanceof Button) {
+            Button clickedButton = (Button) event.getSource();
+            if (clickedButton.getId().equals("add_button")) {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("work_cpt.fxml"));
+                Parent root = null;
+                try {
+                    root = loader.load();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
 
+                Stage newStage = new Stage();
+                newStage.setScene(new Scene(root));
+                newStage.show();
+            }
+        }
+
+        String week = week_button.getText();
+        String start = ftime_button.getText();
+        String end = etime_button.getText();
+        String content = content_textfield.getText();
+        int time = Integer.parseInt(end) - Integer.parseInt(start);
+
+        try {
+            // 서버의 PHP 스크립트 URL로 설정
+            String serverURL = "http://hbr2024.dothome.co.kr/IdFinder.php";
+
+            URL url = new URL(serverURL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+
+            String postData = "userName=" + userName + "&phoneNumber=" + phoneNumber;
+            OutputStream os = connection.getOutputStream();
+            os.write(postData.getBytes("UTF-8"));
+            os.close();
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+
+                // 서버 응답 처리
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String inputLine;
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+
+                System.out.println("Server Response: " + response.toString());
+
+            } else {
+                // 에러 처리
+                System.out.println("HTTP Error Code: " + responseCode);
+            }
+
+            connection.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
 
 }
