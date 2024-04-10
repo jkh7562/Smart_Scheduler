6ed566ac3793034c1824c01fa5db0ce5f7c96ae4
최종수 <cjskkkk@naver.com>
2024-04-11 03:37:39 +0900

    자외선 지수에 따흔 썬크림 추천 완료

--- src/main/java/com/example/smart_scheduler/MainController.java
+++ src/main/java/com/example/smart_scheduler/MainController.java
@@ -25,6 +25,7 @@ import java.net.URLEncoder;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class MainController {
@@ -53,7 +54,7 @@ public class MainController {
     @FXML
     private Label itemLabel3;
 
-
+    private static int UV;
     private static String TMP;
     private static String UUU;
     private static String VVV;
@@ -70,6 +71,7 @@ public class MainController {
     private static String TMN;
     private static int popint;
     private static boolean itememg1;
+
 
     private static String weatherPic;
 
@@ -124,16 +126,31 @@ public class MainController {
         String baseTime = secondClosestTime.format(timeFormatter);
         System.out.println(baseTime);
 
+        //자외선 지수 api 를 위한 시간 값
+        LocalDateTime Now = LocalDateTime.now();
+
+        // 원하는 형식의 포맷터 생성
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHH");
+
+        // 포맷터를 사용하여 날짜와 시간을 지정된 형식으로 출력
+        String formattedDateTime = Now.format(formatter);
+
+
+
 
         // getApiResponse 함수 호출하여 현재 시간의 데이터 가져오기
         try {
             String responseData = getApiResponse(baseDate, baseTime);//전체 단기예보 데이터
             System.out.println("----------------------------------지금 부터는 최고 최저기온 API------------------------------------");
             String responseTM = getApiTM(baseDate, "0200"); //TMX 와 TMN 을 위한 데이터
+            System.out.println("----------------------------------지금 부터는 최고 자외선 API------------------------------------");
+            String responseUV = UVapi(formattedDateTime);
             // 가져온 데이터를 파싱하여 출력
             parseResponse(responseData);
             System.out.println("SKY 값 =" + SKY);
             parseTM(responseTM);
+            parseUV(responseUV);
+
 
             //최고, 최저 기온업데이트
             System.out.println("TMX 값"+TMX +"TMN값" + TMN);
@@ -165,7 +182,7 @@ public class MainController {
     private static String getApiResponse(String baseDate, String baseTime) throws IOException {  // 현재 시간을 기반한 api 데이터
         System.out.println(baseTime);
         StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"); /*URL*/
-        urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=d7Mf01XLXPGGPlYQ8Itqst7R%2FFwiXNOaxoRkW39DZwzgXLW7SnyZ85l73m%2BOcmsY%2FXYWYQYjMQnbYJmNLHtD%2Fg%3D%3D"); /*Service Key*/
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=Hf9Z%2B7fF9N0l2bEfuOAvHN1sxacStyg1c2OdJHO6XE80dGsvI3ms50Wg5pz638TSwTPVy5z%2FPoIe2ex1dCjvyQ%3D%3D"); /*Service Key*/
         urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
         urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("500", "UTF-8")); /*한 페이지 결과 수*/
         urlBuilder.append("&" + URLEncoder.encode("dataType", "UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*요청자료형식(XML/JSON) Default: XML*/
@@ -201,7 +218,7 @@ public class MainController {
     private static String getApiTM(String baseDate, String baseTime) throws IOException{
         System.out.println(baseTime);
         StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"); /*URL*/
-        urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=d7Mf01XLXPGGPlYQ8Itqst7R%2FFwiXNOaxoRkW39DZwzgXLW7SnyZ85l73m%2BOcmsY%2FXYWYQYjMQnbYJmNLHtD%2Fg%3D%3D"); //*Service Key*//*
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=Hf9Z%2B7fF9N0l2bEfuOAvHN1sxacStyg1c2OdJHO6XE80dGsvI3ms50Wg5pz638TSwTPVy5z%2FPoIe2ex1dCjvyQ%3D%3D"); //*Service Key*//*
         urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); //*페이지번호*//*
         urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("500", "UTF-8")); //*한 페이지 결과 수*//*
         urlBuilder.append("&" + URLEncoder.encode("dataType", "UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); //*요청자료형식(XML/JSON) Default: XML*//*
@@ -415,42 +432,69 @@ public class MainController {
         }
     }
 
-    public class UVapi //자외선 지수
-    {
-        public static void main(String[] args) throws IOException
-        {
-            StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/LivingWthrIdxServiceV4/getUVIdxV4"); /*URL*/
-            urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=서비스키"); /*Service Key*/
-            urlBuilder.append("&" + URLEncoder.encode("serviceKey","UTF-8") + "=" + URLEncoder.encode("d7Mf01XLXPGGPlYQ8Itqst7R%2FFwiXNOaxoRkW39DZwzgXLW7SnyZ85l73m%2BOcmsY%2FXYWYQYjMQnbYJmNLHtD%2Fg%3D%3D", "UTF-8")); /*공공데이터포털에서 받은 인증키*/
-            urlBuilder.append("&" + URLEncoder.encode("areaNo","UTF-8") + "=" + URLEncoder.encode("1100000000", "UTF-8")); /*서울지점*/
-            urlBuilder.append("&" + URLEncoder.encode("time","UTF-8") + "=" + URLEncoder.encode("2022071118", "UTF-8")); /*2022년7월11일18시*/
-            urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode("xml", "UTF-8")); /*xml, json 선택(미입력시 xml)*/
-            URL url = new URL(urlBuilder.toString());
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Content-type", "application/json");
-            System.out.println("Response code: " + conn.getResponseCode());
-            BufferedReader rd;
+    public static String UVapi(String formattedDateTime) throws IOException{ //자외선 지수
+        //public static void main(String[] args) throws IOException
 
-            if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300)
-            {
-                rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            } else
-            {
-                rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/LivingWthrIdxServiceV4/getUVIdxV4"); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=Hf9Z%2B7fF9N0l2bEfuOAvHN1sxacStyg1c2OdJHO6XE80dGsvI3ms50Wg5pz638TSwTPVy5z%2FPoIe2ex1dCjvyQ%3D%3D"); /*Service Key*/
+        //urlBuilder.append("&" + URLEncoder.encode("serviceKey","UTF-8") + "=" + URLEncoder.encode("Hf9Z%2B7fF9N0l2bEfuOAvHN1sxacStyg1c2OdJHO6XE80dGsvI3ms50Wg5pz638TSwTPVy5z%2FPoIe2ex1dCjvyQ%3D%3D", "UTF-8")); /*공공데이터포털에서 받은 인증키*/
+        urlBuilder.append("&" + URLEncoder.encode("areaNo","UTF-8") + "=" + URLEncoder.encode("4420033000", "UTF-8")); /*아산지점*/
+        urlBuilder.append("&" + URLEncoder.encode("time","UTF-8") + "=" + URLEncoder.encode(formattedDateTime, "UTF-8")); /*2022년7월11일18시*/
+        urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*xml, json 선택(미입력시 xml)*/
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        System.out.println("Response code: " + conn.getResponseCode());
+        BufferedReader rd;
+
+        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300)
+        {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else
+        {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null)
+        {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+        System.out.println(sb.toString());
+        return sb.toString();
+    }
+    private static void parseUV(String responseData) {
+        try {
+            JSONObject jsonObject = new JSONObject(responseData);
+            JSONObject response = jsonObject.getJSONObject("response");
+            JSONObject body = response.getJSONObject("body");
+            JSONObject items = body.getJSONObject("items");
+            JSONArray itemList = items.getJSONArray("item");
+
+            // h0부터 h24까지의 값을 비교하여 가장 높은 값을 찾음
+            int highestValue = Integer.MIN_VALUE;
+            for (int i = 0; i < itemList.length(); i++) {
+                JSONObject item = itemList.getJSONObject(i);
+                for (int j = 0; j <= 24; j++) {
+                    String key = "h" + j;
+                    if (item.has(key)) {
+                        int value = item.getInt(key);
+                        highestValue = Math.max(highestValue, value);
+                    }
+                }
             }
-            StringBuilder sb = new StringBuilder();
-            String line;
-            while ((line = rd.readLine()) != null)
-            {
-                sb.append(line);
-            }
-            rd.close();
-            conn.disconnect();
-            System.out.println(sb.toString());
+
+            // 가장 높은 값을 변수에 저장
+            UV = highestValue;
+            System.out.println("가장 높은 값: " + UV);
+        }catch (JSONException e) {
+            e.printStackTrace();
+
         }
     }
-
 
 
     @FXML  //날씨 사진 바꾸기
@@ -462,7 +506,7 @@ public class MainController {
             case "1":
                 imagePath = "001lighticons-02.png"; // 맑은 날씨 이미지 경로
                 updateWeatherItem1();//비가 올 경우 우산 추천
-                itemImg2 = "001lighticons-18.png";  //추천 아이템 2
+                updateWeatherItem2();  //자외선이 쎌 경우 썬크림 추천
                 itemImg3 = "001lighticons-18.png";  //추천아이템 3
                 weather.setImage(new Image(imagePath));
                 weatherLabel.setText("맑음");
@@ -470,64 +514,58 @@ public class MainController {
             case "2":
                 imagePath = "001lighticons-18.png"; // 비 오는 날씨 이미지 경로
                 itemImg1 = "um.png"; //추천 아이템 1
-                itemImg2 = "001lighticons-18.png";  //추천 아이템 2
+                updateWeatherItem2();  //자외선이 쎌 경우 썬크림 추천
                 itemImg3 = "001lighticons-18.png";  //추천아이템 3
                 weather.setImage(new Image(imagePath));
                 weatherLabel.setText("비");
                 item1.setImage(new Image(itemImg1));
-                item2.setImage(new Image(itemImg2));
                 item3.setImage(new Image(itemImg3));
                 break;
             case "3":
                 imagePath = "001lighticons-08.png"; // 구름 많음
                 updateWeatherItem1();//오늘 비가 올경우 우산 추천
-                itemImg2 = "001lighticons-18.png";  //추천 아이템 2
+                updateWeatherItem2();  //자외선이 쎌 경우 썬크림 추천
                 itemImg3 = "001lighticons-18.png";  //추천아이템 3
                 weather.setImage(new Image(imagePath));
                 weatherLabel.setText("흐림");
-                item2.setImage(new Image(itemImg2));
                 item3.setImage(new Image(itemImg3));
                 break;
             case "4":
                 imagePath = "001lighticons-14.png"; // 흐림 날씨 이미지 경로
                 updateWeatherItem1(); //추천 아이템 1
-                itemImg2 = "001lighticons-18.png";  //추천 아이템 2
+                updateWeatherItem2();  //자외선이 쎌 경우 썬크림 추천
                 itemImg3 = "001lighticons-18.png";  //추천아이템 3
                 weather.setImage(new Image(imagePath));
                 weatherLabel.setText("구름");
-                item2.setImage(new Image(itemImg2));
                 item3.setImage(new Image(itemImg3));
                 break;
             case "5":
                 imagePath = "001lighticons-08.png"; // 비와 눈이 같이 오는 날씨 이미지 경로
                 updateWeatherItem1();
-                itemImg2 = "001lighticons-18.png";  //추천 아이템 2
+                updateWeatherItem2();  //자외선이 쎌 경우 썬크림 추천
                 itemImg3 = "001lighticons-18.png";  //추천아이템 3
                 weather.setImage(new Image(imagePath));
                 weatherLabel.setText("진눈깨비");
-                item2.setImage(new Image(itemImg2));
                 item3.setImage(new Image(itemImg3));
                 break;
             case "6":
                 imagePath = "001lighticons-24.png"; // 눈 오는 날씨 이미지 경로
                 updateWeatherItem1();
-                itemImg2 = "001lighticons-18.png";  //추천 아이템 2
+                updateWeatherItem2();  //자외선이 쎌 경우 썬크림 추천
                 itemImg3 = "001lighticons-18.png";  //추천아이템 3
                 weather.setImage(new Image(imagePath));
                 weatherLabel.setText("눈");
-                item2.setImage(new Image(itemImg2));
                 item3.setImage(new Image(itemImg3));
                 break;
             default:
                 // 아무것도 해당되지 않을 때의 기본 이미지 설정
                 imagePath = "001lighticons-08.png"; // 기본 이미지 경로
                 itemImg1 = "001lighticons-18.png"; //추천 아이템 1
-                itemImg2 = "001lighticons-18.png";  //추천 아이템 2
+                updateWeatherItem2();  //자외선이 쎌 경우 썬크림 추천
                 itemImg3 = "001lighticons-18.png";  //추천아이템 3
                 weather.setImage(new Image(imagePath));
-                weatherLabel.setText("알수없음");
+                weatherLabel.setText("오류");
                 item1.setImage(new Image(itemImg1));
-                item2.setImage(new Image(itemImg2));
                 item3.setImage(new Image(itemImg3));
                 break;
         }
@@ -538,6 +576,11 @@ public class MainController {
     {
         hightemp.setText(TMX);
         lowtemp.setText(TMN);
+    }
+
+    @FXML
+    public void updateUVtext(int uv){
+
     }
 
     @FXML//비가 올 확률이 40% 이상인 경우 우산아이템 추천
@@ -554,6 +597,19 @@ public class MainController {
         }
     }
 
+    @FXML//자외선 지수가 5 이상이면 선크림 추천
+    public void updateWeatherItem2(){
+        String itemImg2;
+        if(UV >= 5){
+            itemImg2 = "sun.png";
+            item2.setImage(new Image(itemImg2));
+            itemLabel2.setText("썬크림");
+        }else {
+            itemImg2 = "";
+            item2.setImage(new Image(itemImg2));
+            itemLabel2.setText("");
+        }
+    }
 
     @FXML
     private void workButtonAction(ActionEvent event) {
