75f0028ecf4dd9a953491beef166a4cc5cd40bfc
jkh7562 <86720157+jkh7562@users.noreply.github.com>
2024-05-13 21:31:45 +0900

    수정

--- src/main/java/com/example/smart_scheduler/projectteamnameController.java
+++ src/main/java/com/example/smart_scheduler/projectteamnameController.java
@@ -1,18 +1,45 @@
 package com.example.smart_scheduler;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class projectteamnameController {
     @FXML
     ImageView back_image;
+    @FXML
+    TextField teamname;
+    @FXML
+    Button okay_button;
     @FXML
     private void backimageClicked(MouseEvent event) {
         // 현재 창(Stage)을 가져옵니다.
         Stage currentStage = (Stage) back_image.getScene().getWindow();
         // 현재 창을 닫습니다.
         currentStage.close();
+    }
+
+    @FXML
+    private void okayButtonAction(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("teamname_cpt.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
