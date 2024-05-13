a5543bedcaf00a897d0dc884ff6313e494a79b89
jkh7562 <86720157+jkh7562@users.noreply.github.com>
2024-05-13 22:57:55 +0900

    수정

--- src/main/java/com/example/smart_scheduler/projectmanageController.java
+++ src/main/java/com/example/smart_scheduler/projectmanageController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
@@ -15,6 +16,11 @@ import java.io.IOException;
 public class projectmanageController {
     @FXML
     ImageView back_image;
+    @FXML
+    Button change_button;
+    @FXML
+    Label project_label;
+
     @FXML
     private void backimageClicked(MouseEvent event) {
         // 현재 창을 닫음
@@ -36,6 +42,21 @@ public class projectmanageController {
             Stage newStage = new Stage();
             newStage.setScene(new Scene(root));
             newStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void changeButtonAction(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("project_name.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
