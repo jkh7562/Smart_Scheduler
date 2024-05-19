1b2ce3d6cd3e53685328435cdf2158bd3011b08f
jkh7562 <86720157+jkh7562@users.noreply.github.com>
2024-05-19 12:43:34 +0900

    수정

--- src/main/java/com/example/smart_scheduler/projectteamController.java
+++ src/main/java/com/example/smart_scheduler/projectteamController.java
@@ -1,9 +1,13 @@
 package com.example.smart_scheduler;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -49,10 +53,28 @@ public class projectteamController {
 
     @FXML
     private void backimageClicked(MouseEvent event) {
-        // 현재 창(Stage)을 가져옵니다.
+        // 현재 창을 닫음
         Stage currentStage = (Stage) back_image.getScene().getWindow();
-        // 현재 창을 닫습니다.
         currentStage.close();
+
+        // 다른 열려 있는 창들을 닫음
+        Stage[] openStages = Stage.getWindows().toArray(new Stage[0]);
+        for (Stage stage : openStages) {
+            if (stage != currentStage) {
+                stage.close();
+            }
+        }
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Main_Project.fxml"));
+            Parent root = loader.load();
+
+            Stage newStage = new Stage();
+            newStage.setScene(new Scene(root));
+            newStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public String primary() {
@@ -142,6 +164,53 @@ public class projectteamController {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        String teamname = team_label.getText();
+
+        try {
+            String serverURL = "http://hbr2024.dothome.co.kr/getteam.php";
+            URL url = new URL(serverURL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+
+            String postData = "teamname=" + teamname;
+            OutputStream os = connection.getOutputStream();
+            os.write(postData.getBytes("UTF-8"));
+            os.close();
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String inputLine;
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+
+                in.close();
+                System.out.println("Response: " + response.toString());
+
+                // JSON 파싱 및 ListView에 추가
+                JSONArray jsonArray = new JSONArray(response.toString());
+                Platform.runLater(() -> {
+                    team_listview.getItems().clear(); // 기존 항목을 초기화
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        String userID = jsonObject.getString("userID");
+                        String userName = jsonObject.getString("userName");
+                        String displayText = userName + " - " + userID;
+                        team_listview.getItems().add(displayText);
+                    }
+                });
+
+
+            } else {
+                System.out.println("HTTP Error Code: " + responseCode);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
@@ -185,6 +254,129 @@ public class projectteamController {
             }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void addButtonClick(ActionEvent event) {
+        String selectedItem = search_listview.getSelectionModel().getSelectedItem();
+        String teamname = team_label.getText();
+
+        if (selectedItem != null) {
+            // userName - userID 형식에서 userID 추출
+            String[] parts = selectedItem.split(" - ");
+            if (parts.length == 2) {
+                String userName = parts[0];
+                String userID = parts[1];
+
+                // userID를 사용한 작업
+                System.out.println("Extracted userID: " + userID);
+
+                try {
+                    String serverURL = "http://hbr2024.dothome.co.kr/teamadd.php";
+                    URL url = new URL(serverURL);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("POST");
+                    connection.setDoOutput(true);
+
+                    String postData = "Id=" + userID + "&teamname=" + teamname;
+                    OutputStream os = connection.getOutputStream();
+                    os.write(postData.getBytes("UTF-8"));
+                    os.close();
+
+                    int responseCode = connection.getResponseCode();
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+                        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                        StringBuilder response = new StringBuilder();
+                        String inputLine;
+
+                        while ((inputLine = in.readLine()) != null) {
+                            response.append(inputLine);
+                        }
+
+                        in.close();
+                        System.out.println("Response: " + response.toString());
+                        try {
+                            FXMLLoader loader = new FXMLLoader(getClass().getResource("teamadd_cpt.fxml"));
+                            Parent root = loader.load();
+                            Scene scene = new Scene(root);
+                            Stage stage = new Stage();
+                            stage.setScene(scene);
+                            stage.show();
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    } else {
+                        System.out.println("HTTP Error Code: " + responseCode);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("Selected item format is incorrect.");
+            }
+        }
+    }
+
+    @FXML
+    private void deleteButtonClick(ActionEvent event) {
+        String selectedItem = team_listview.getSelectionModel().getSelectedItem();
+
+        if (selectedItem != null) {
+            // userName - userID 형식에서 userID 추출
+            String[] parts = selectedItem.split(" - ");
+            if (parts.length == 2) {
+                String userName = parts[0];
+                String userID = parts[1];
+
+                // userID를 사용한 작업
+                System.out.println("Extracted userID: " + userID);
+                try {
+                    String serverURL = "http://hbr2024.dothome.co.kr/teamdelete.php";
+                    URL url = new URL(serverURL);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("POST");
+                    connection.setDoOutput(true);
+
+                    String postData = "Id=" + userID;
+                    OutputStream os = connection.getOutputStream();
+                    os.write(postData.getBytes("UTF-8"));
+                    os.close();
+
+                    int responseCode = connection.getResponseCode();
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+                        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                        StringBuilder response = new StringBuilder();
+                        String inputLine;
+
+                        while ((inputLine = in.readLine()) != null) {
+                            response.append(inputLine);
+                        }
+
+                        in.close();
+                        System.out.println("Response: " + response.toString());
+                        try {
+                            FXMLLoader loader = new FXMLLoader(getClass().getResource("teamdelete_cpt.fxml"));
+                            Parent root = loader.load();
+                            Scene scene = new Scene(root);
+                            Stage stage = new Stage();
+                            stage.setScene(scene);
+                            stage.show();
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    } else {
+                        System.out.println("HTTP Error Code: " + responseCode);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
